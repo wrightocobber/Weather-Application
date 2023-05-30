2237fc6ada42659dd44fa2cba29607156b2ea0ef
wrightocobber <gemcwright91@hotmail.com>
2023-05-30 12:59:52 +0800

    Change Icon using API

--- Weather App Code/src/app.js
+++ Weather App Code/src/app.js
@@ -27,20 +27,25 @@ function formatDate(timestamp) {
 function showTemperature(apiResponse) {
   console.log(apiResponse);
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = apiResponse.data.city;
   let tempElement = document.querySelector("#temperature");
-  tempElement.innerHTML = Math.round(apiResponse.data.temperature.current);
   let conditionElement = document.querySelector("#condition");
-  conditionElement.innerHTML = apiResponse.data.condition.description;
+  let iconElement = document.querySelector("#icon");
   let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = apiResponse.data.temperature.humidity;
   let wind = document.querySelector("#wind");
-  wind.innerHTML = Math.round(apiResponse.data.wind.speed);
   let date = document.querySelector("#date");
+
+  cityElement.innerHTML = apiResponse.data.city;
+  tempElement.innerHTML = Math.round(apiResponse.data.temperature.current);
+  conditionElement.innerHTML = apiResponse.data.condition.description;
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${apiResponse.data.condition.icon}.png`
+  );
+  humidity.innerHTML = apiResponse.data.temperature.humidity;
+  wind.innerHTML = Math.round(apiResponse.data.wind.speed);
   date.innerHTML = formatDate(apiResponse.data.time * 1000);
 }
 let apiKey = "78af09ec4f1b3eda1a73782o76t19456";
 let city = "Sydney";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric;`;
-console.log(apiUrl);
 axios.get(apiUrl).then(showTemperature);
